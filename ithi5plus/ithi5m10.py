86f768ecfeea3249b9ebae79c73b0391dc98953e
Christian Huitema <huitema@huitema.net>
2022-05-31 15:40:06 -0700

    Automate M10 metric production

--- ithi5plus/ithi5m10.py
+++ ithi5plus/ithi5m10.py
@@ -3,6 +3,7 @@ import sys
 import ithi5file
 import os
 from os.path import isfile, isdir, join
+import datetime
 
 class m10_as_per_cc :
     def __init__(self, as_text):
@@ -83,29 +84,85 @@ class m10_per_country:
             else:
                 self.open_dns[odns] = other.open_dns[odns]
 
+def last_day_of_month(s_year, s_month):
+    year = int(s_year)
+    month = int(s_month)
+    first_day_next_month = datetime.date(year, month, 27)
+    last_day_this_month = first_day_next_month
+    while first_day_next_month.month == month:
+        last_day_this_month = first_day_next_month
+        first_day_next_month += datetime.timedelta(1)
+    return last_day_this_month.day
+
+def buildm10(source_folder, target_folder, year, month):
+    # compute the last day of the month
+    # Load the files for dir/year/month/*
+    cc_data_list = dict()
+    last_day = last_day_of_month(year, month)
+    source_year = join(source_folder, year)
+    if not isdir(source_year):
+        print("Not a directory: " + source_year)
+        return
+    source_month = join(source_year, month)
+    if not isdir(source_month):
+        print("Not a directory: " + source_month)
+        return
+    for day in range(1,last_day + 1):
+        s_day = str(day)
+        if day < 10:
+            s_day = "0" + str(day)
+        source_day = join(source_year, s_day)
+        if isdir(source_day):
+            for p in ithi5file.build_file_list(source_day):
+                ithi5 = ithi5file.ithi5plus_file(p, year, month, s_day)
+                ithi5.load_file()
+                for i5pe in ithi5.entries:
+                    if not i5pe.cc in cc_data_list:
+                        cc_data_list[i5pe.cc] = m10_per_country(i5pe.cc)
+                    cc_data_list[i5pe.cc].load(i5pe)
+    if len(cc_data_list) == 0:
+        print("Could not find any file in " + source_month)
+        return
+    # Sum of all the files for the month
+    zz_data =  m10_per_country('ZZ')
+    for cc in cc_data_list:
+        zz_data.add(cc_data_list[cc])
+    # Write the metrics:
+    metric_day = year + month + str(last_day)
+    metric_file_name = "M10-" + metric_day + ".csv"
+    metric_file_path = join(target_folder, metric_file_name)
+    with open(metric_file_path,"wt") as F:
+        zz_data.write_m10(F,metric_day)
+        for cc in cc_data_list:
+            cc_data_list[cc].write_m10(F,metric_day)
+    print("Saved " + str(len(cc_data_list)) + " in " + metric_file_path)
+
+def usage():
+    print("Usage:")
+    print("    ithi5m10.py source_folder target_folder [ <year> <month> ]")
+    exit(1)
+
 # Main code
 # Read year, month, day from command line.
-year = sys.argv[1]
-month = sys.argv[2]
-day = sys.argv[3]
-# Read main folder from command line.
-folder = sys.argv[4]
-# Load the files for dir/year/month/*
-cc_data_list = dict()
-for p in ithi5file.build_file_list(folder):
-    ithi5 = ithi5file.ithi5plus_file(p, year, month, day)
-    ithi5.load_file()
-    for i5pe in ithi5.entries:
-        if not i5pe.cc in cc_data_list:
-            cc_data_list[i5pe.cc] = m10_per_country(i5pe.cc)
-        cc_data_list[i5pe.cc].load(i5pe)
-# Sum of all the files for the month
-zz_data =  m10_per_country('ZZ')
-for cc in cc_data_list:
-    zz_data.add(cc_data_list[cc])
-# Write the metrics:
-last_day_of_month = year + "-" + month + "-" + day
-with open(sys.argv[5],"wt") as F:
-    zz_data.write_m10(F,last_day_of_month)
-    for cc in cc_data_list:
-        cc_data_list[cc].write_m10(F,last_day_of_month)
+source_folder = sys.argv[1]
+target_folder = sys.argv[2]
+
+if len(sys.argv) == 5: 
+    year = sys.argv[3]
+    month = sys.argv[4]
+    buildm10(source_folder, target_folder, year, month)
+else:
+    current_day = datetime.datetime.now()
+    year = str(current_day.year)
+    month = str(current_day.month).zfill(2)
+    first_day = datetime.date(current_day.year, current_day.month, 1)
+    delta_one_day = datetime.timedelta(1)
+    previous_day = first_day - delta_one_day
+    previous_year = str(previous_day.year)
+    previous_month = str(previous_day.month).zfill(2)
+    print("Building M10 " + year + "/" + month)
+    buildm10(source_folder, target_folder, year, month)
+    last_day_of_month(previous_year, previous_month)
+    print("Building M10 " + previous_year + "/" + previous_month)
+    buildm10(source_folder, target_folder, previous_year, previous_month)
+    print("Done")
