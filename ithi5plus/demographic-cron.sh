291f9574f15f1e5f25753a9865c869790a2b98a6
Christian Huitema <huitema@huitema.net>
2022-06-01 20:10:46 -0700

    Registering the cron scripts

---
+++ ithi5plus/demographic-cron.sh
@@ -0,0 +1,4 @@
+cd /home/ubuntu/ithi5plus/ithi5plus/
+pwd
+python3 ./demographics.py /home/gih/data/resolvers/ /var/www/html/demographics/
+
---
+++ ithi5plus/m10-cron.sh
@@ -0,0 +1,4 @@
+cd /home/ubuntu/ithi5plus/ithi5plus/
+pwd
+python3 ./ithi5m10.py /home/gih/data/resolvers/ /home/ubuntu/ithi/M10/
+
